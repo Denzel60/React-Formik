a09c5879487a829b87dfc836efc4a37fb6e4cfb0
Denzel <lawrencedenzel60@gmail.com>
2024-06-27 14:54:46 +0300

    "Basics of formik, Updated package-lock.json, package.json, App.jsx, and Form.jsx files"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Form from './form.jsx'
+import Form from './Form.jsx'
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Form />} />
-        </Routes>
-      </BrowserRouter>
+      <Form />
     </>
   )
 }
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,32 @@
+import { useFormik } from 'formik'
+
 function Form() {
+    const formik = useFormik({
+        initialValues: {
+            firstName: "",
+            lastName: "",
+            email: ""
+        },
+        onSubmit: (formState) => {
+            console.log(formState)
+        }
+    })
     return (
-        <div>
-            <h1>FORM NOW</h1>
-        </div>
+        <form onSubmit={formik.handleSubmit}>
+            <div>
+                <label htmlFor="firstName">Enter first name</label>
+                <input type="text" id="firstName" name='firstName' value={formik.values.firstName} onChange={formik.handleChange} />
+            </div>
+            <div>
+                <label htmlFor="lastName">Enter last name</label>
+                <input type="text" id="lastName" name='lastName' value={formik.values.lastName} onChange={formik.handleChange} />
+            </div>
+            <div>
+                <label htmlFor="email">Enter email</label>
+                <input type="text" id="email" name='email' value={formik.values.email} onChange={formik.handleChange} />
+            </div>
+            <button type='submit'>Submit</button>
+        </form>
     )
 }
 
