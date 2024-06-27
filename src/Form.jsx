56aac2f2e0c0eb2494296ba1549c4e5629386080
Denzel <lawrencedenzel60@gmail.com>
2024-06-27 15:27:49 +0300

    "Added form validation and error display for first name, last name, and email fields"

--- src/Form.jsx
+++ src/Form.jsx
@@ -9,6 +9,27 @@ function Form() {
         },
         onSubmit: (formState) => {
             console.log(formState)
+        },
+        validate: (formValues) => {
+            let errors = {};
+
+            if (formValues.firstName === "") {
+                errors.firstName = "First name is required"
+            } else if (formValues.firstName.length < 4) {
+                errors.firstName = "First name must be at least 8 characters"
+            }
+
+            if (formValues.lastNamestName === "") {
+                errors.lastName = "Last name is required"
+            } else if (formValues.lastName.length < 4) {
+                errors.lastName = "Last name must be at least 8 characters"
+            }
+
+            if (formValues.email === "") {
+                errors.email = "email is required"
+            }
+
+            return errors
         }
     })
     return (
@@ -16,14 +37,17 @@ function Form() {
             <div>
                 <label htmlFor="firstName">Enter first name</label>
                 <input type="text" id="firstName" name='firstName' value={formik.values.firstName} onChange={formik.handleChange} />
+                {formik.errors.firstName && <div style={{ color: "red" }}>{formik.errors.firstName}</div>}
             </div>
             <div>
                 <label htmlFor="lastName">Enter last name</label>
                 <input type="text" id="lastName" name='lastName' value={formik.values.lastName} onChange={formik.handleChange} />
+                {formik.errors.lastName && <div style={{ color: "red" }}>{formik.errors.lastName}</div>}
             </div>
             <div>
                 <label htmlFor="email">Enter email</label>
-                <input type="text" id="email" name='email' value={formik.values.email} onChange={formik.handleChange} />
+                <input type="email" id="email" name='email' value={formik.values.email} onChange={formik.handleChange} />
+                {formik.errors.email && <div style={{ color: "red" }}>{formik.errors.email}</div>}
             </div>
             <button type='submit'>Submit</button>
         </form>
