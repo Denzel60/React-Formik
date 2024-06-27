e6cec00277b4a7920c103111cabcf9d5ecf2bfb6
Denzel <lawrencedenzel60@gmail.com>
2024-06-27 16:08:52 +0300

    "Added yup validation to Form component, updated package.json and package-lock.json to include yup dependency"

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,6 +1,23 @@
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 function Form() {
+    const validatonSchema = Yup.object({
+        firstName: Yup.string()
+            .min(4, "First name must be at least 4 character")
+            .max(15, "First name can not exceed 15 characters")
+            .required("First name is required"),
+
+        lastName: Yup.string()
+            .min(4, "Last name must be at least 4 character")
+            .max(15, "Last name can not exceed 15 characters")
+            .required("Last name is required"),
+
+        email: Yup.string()
+            .email("Invalid email")
+            .required("Email is required")
+    })
+
     const formik = useFormik({
         initialValues: {
             firstName: "",
@@ -10,44 +27,47 @@ function Form() {
         onSubmit: (formState) => {
             console.log(formState)
         },
-        validate: (formValues) => {
-            let errors = {};
 
-            if (formValues.firstName === "") {
-                errors.firstName = "First name is required"
-            } else if (formValues.firstName.length < 4) {
-                errors.firstName = "First name must be at least 8 characters"
-            }
+        validationSchema: validatonSchema,
 
-            if (formValues.lastNamestName === "") {
-                errors.lastName = "Last name is required"
-            } else if (formValues.lastName.length < 4) {
-                errors.lastName = "Last name must be at least 8 characters"
-            }
+        // validate: (formValues) => {
+        //     let errors = {};
 
-            if (formValues.email === "") {
-                errors.email = "email is required"
-            }
+        //     if (formValues.firstName === "") {
+        //         errors.firstName = "First name is required"
+        //     } else if (formValues.firstName.length < 4) {
+        //         errors.firstName = "First name must be at least 8 characters"
+        //     }
 
-            return errors
-        }
+        //     if (formValues.lastNamestName === "") {
+        //         errors.lastName = "Last name is required"
+        //     } else if (formValues.lastName.length < 4) {
+        //         errors.lastName = "Last name must be at least 8 characters"
+        //     }
+
+        //     if (formValues.email === "") {
+        //         errors.email = "email is required"
+        //     }
+
+        //     return errors
+        // },
     })
     return (
         <form onSubmit={formik.handleSubmit}>
             <div>
                 <label htmlFor="firstName">Enter first name</label>
-                <input type="text" id="firstName" name='firstName' value={formik.values.firstName} onChange={formik.handleChange} />
-                {formik.errors.firstName && <div style={{ color: "red" }}>{formik.errors.firstName}</div>}
+                <input type="text" id="firstName" name='firstName' value={formik.values.firstName} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                {formik.touched.firstName && formik.errors.firstName && <div style={{ color: "red" }}>{formik.errors.firstName}</div>}
             </div>
             <div>
                 <label htmlFor="lastName">Enter last name</label>
-                <input type="text" id="lastName" name='lastName' value={formik.values.lastName} onChange={formik.handleChange} />
-                {formik.errors.lastName && <div style={{ color: "red" }}>{formik.errors.lastName}</div>}
+                <input type="text" id="lastName" name='lastName' value={formik.values.lastName} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                {formik.touched.lastName && formik.errors.lastName && <div style={{ color: "red" }}>{formik.errors.lastName}</div>}
             </div>
             <div>
                 <label htmlFor="email">Enter email</label>
-                <input type="email" id="email" name='email' value={formik.values.email} onChange={formik.handleChange} />
-                {formik.errors.email && <div style={{ color: "red" }}>{formik.errors.email}</div>}
+                <input type="email" id="email" name='email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                {formik.touched.email && formik.errors.email && <div style={{ color: "red" }}>{formik.errors.email}</div>}
             </div>
             <button type='submit'>Submit</button>
         </form>
